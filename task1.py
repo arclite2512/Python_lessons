24e1512b6a5c9858f08e4546523a5de44dfd6d51
ChernovAA <arclite@bk.ru>
2022-08-04 21:03:16 +0300

    выполнил 1 задачу

---
+++ task1.py
@@ -0,0 +1,20 @@
+str_var = "Первая переменная"
+int_var = 531
+float_var = 7.67
+list_var = [3, 7, 'текст']
+
+print(str_var)
+print(int_var)
+print(float_var)
+print(list_var)
+
+dig_1 = int(input('Введите целое число: '))
+dig_2 = int(input('Введите целое число: '))
+dig_3 = int(input('Введите целое число: '))
+
+str_1 = input('Введите строку: ')
+str_2 = input('Введите строку: ')
+str_3 = input('Введите строку: ')
+
+print("Вы ввели числа:", dig_1, dig_2, dig_3)
+print("Вы ввели строки:", str_1, str_2, str_3)
