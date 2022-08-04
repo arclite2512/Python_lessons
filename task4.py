8f9f2ad3fe3532df91831d8e17abd33ccfb27f84
ChernovAA <arclite@bk.ru>
2022-08-04 21:34:28 +0300

    выполнил задачу 4

---
+++ task4.py
@@ -0,0 +1,14 @@
+n = int(input("Введите целое положительное число "))
+max1 = n % 10
+print(max1)
+while n >= 1:
+    n = n // 10
+    print(n)
+    if n % 10 > max1:
+        print(n)
+        print(max1)
+        max1 = n % 10
+        print(max1)
+    elif n > 9:
+        break
+print("Максимальное цифра в числе ", max1)
